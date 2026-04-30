70a110854ffb8e61cecd39cbb1028b8df081f178
deborah <fabbydebby@gmail.com>
2026-05-01 01:33:07 +0300

    fix-all-ts-errors

--- src/components/NavLink.tsx
+++ src/components/NavLink.tsx
@@ -1,4 +1,4 @@
-import { NavLink, type NavLinkProps } from "react-router-dom";
+import { NavLink as RouterNavLink, type NavLinkProps } from "react-router-dom";
 import { forwardRef } from "react";
 import { cn } from "@/lib/utils";
 
--- src/components/ui/pagination.tsx
+++ src/components/ui/pagination.tsx
@@ -1,8 +1,9 @@
 import * as React from "react";
 import { ChevronLeft, ChevronRight, MoreHorizontal } from "lucide-react";
+import { type VariantProps } from "class-variance-authority";
 
 import { cn } from "@/lib/utils";
-import { type VariantProps, buttonVariants } from "@/components/ui/button";
+import { buttonVariants } from "@/components/ui/button";
 
 const Pagination = ({ className, ...props }: React.ComponentProps<"nav">) => (
   <nav
